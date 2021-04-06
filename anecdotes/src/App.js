986fae3c9dc3dbaf4a5983067d740feffd608a81
jgillespie <jgillespie17@wou.edu>
2021-04-05 19:28:28 -0700

    Finished 1.14

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -7,7 +7,27 @@ const CountVote = (props) => {
   console.log(props.array)
   return copy
 }
-const Display = (props) => <p>has {props.value} votes</p>
+
+const DisplayLargest = (props) => {
+  const total = props.value.array.reduce((result,number)=> result+number);
+ 
+  if(total === 0){
+    return(
+      <>
+    <h2>Anecdote with most votes</h2>
+    </>
+    )
+  }
+  const maxValue = Math.max(...props.value.array)
+  const maxItem = props.value.array.findIndex(element => element === maxValue);
+  return(
+    <>
+    <h2>Anecdote with most votes</h2>
+    <p>{props.anecdotes[maxItem]}</p>
+    <p>has {maxValue} votes</p>
+    </>
+  )
+}
 
 const App = () => {
   const anecdotes = [
@@ -18,18 +38,19 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
-  const [votes, setVotes] = useState(Array.apply(null, new Array(6)).map(Number.prototype.valueOf, 0))
+  const [votes] = useState(Array.apply(null, new Array(6)).map(Number.prototype.valueOf, 0))
   const [selected, setSelected] = useState(0)
   const values = {array : votes, selected: selected}
 
   return (
 
     <div>
+      <h2>Anecdote of the Day</h2>
       <p>{anecdotes[selected]}</p>
-      <Display value={votes[selected]}/>
-
+      <p>has {votes[selected]} vote(s)</p>
       <button onClick={() => {CountVote(values)}}>Vote</button>
       <button onClick={() => setSelected(Math.floor((Math.random() * 6) ))}>next anecdote</button>
+      <DisplayLargest value={values} anecdotes={anecdotes} />
     </div>
   )
   

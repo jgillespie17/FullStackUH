e78335361818dc9fb2337a5b7c6a3e8e94931162
jgillespie <jgillespie17@wou.edu>
2021-04-05 18:34:37 -0700

    Finished 1.13, couldnt get number on view to update after button click

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -1,5 +1,14 @@
 import React, { useState } from 'react'
 
+const CountVote = (props) => {
+  const copy = props.array
+  copy[props.selected] += 1
+  console.log(props.selected)
+  console.log(props.array)
+  return copy
+}
+const Display = (props) => <p>has {props.value} votes</p>
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -9,15 +18,21 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
-   
+  const [votes, setVotes] = useState(Array.apply(null, new Array(6)).map(Number.prototype.valueOf, 0))
   const [selected, setSelected] = useState(0)
+  const values = {array : votes, selected: selected}
 
   return (
+
     <div>
       <p>{anecdotes[selected]}</p>
+      <Display value={votes[selected]}/>
+
+      <button onClick={() => {CountVote(values)}}>Vote</button>
       <button onClick={() => setSelected(Math.floor((Math.random() * 6) ))}>next anecdote</button>
     </div>
   )
+  
 }
 
 export default App

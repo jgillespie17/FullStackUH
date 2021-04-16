10fb14f47fddb74e65e68eb07d0f4891d4efc93d
jgillespie <HERCULESJON123@gmail.com>
2021-04-15 20:11:45 -0700

    Finished 2.10, not 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,64 @@
 import React, { useState } from 'react'
 
+
+
+const DisplayForm = (props) => {
+  return(
+  <form onSubmit={props.addName}>
+        <div>
+          name: <input 
+            value={props.newName}
+            onChange={props.handleNameChange}
+            />
+        </div>
+        <div>
+          number: <input 
+            value={props.newNumber}
+            onChange={props.handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+  )
+
+}
+
+const Display = (props) => {
+  return(
+    <ul>
+        {props.display.map(person =>
+          <Person key={person.name} person={person} />
+          )}
+      </ul>
+  )
+}
+
+const Person = ({person}) => {
+  return (
+    <li>{person.name} {person.number}</li>
+  )
+}
+
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: "040-1234567" }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
 
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+  
   const addName = (event) => {
     event.preventDefault()
 
@@ -23,53 +75,15 @@ const App = () => {
       setPersons(persons.concat(newPerson))
       setNewName('')
     }
-    
-   
-  }
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input 
-            value={newName}
-            onChange={handleNameChange}
-            />
-        </div>
-        <div>
-          number: <input 
-            value={newNumber}
-            onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <DisplayForm newName={newName} newNumber={newNumber} addName={(event) => addName(event)} handleNameChange={(event) => handleNameChange(event)} handleNumberChange={(event) => handleNumberChange(event)} />
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person =>
-          <Person key={person.name} person={person} />
-          )}
-      </ul>
+      <Display display={persons} />
     </div>
   )
 }
-
-const Person = ({person}) => {
-  return (
-    <li>{person.name} {person.number}</li>
-  )
-}
-
-
-
 export default App
